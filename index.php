5ffe9784209beb06421ce20ec12e79881738c471
brayan <brayan@gmail.com>
2024-01-22 12:44:25 -0500

    Cambio index y nuevo archivo Control

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente illum nesciunt maxime impedit voluptatem a tenetur recusandae. Unde eveniet voluptatibus, ad molestiae atque accusantium doloribus odit quasi omnis dolorem similique.</h3>
+</body>
+</html>
