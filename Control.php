6ca5e728057234d0f91c674531b75484eccb8774
brayan <brayan@gmail.com>
2024-01-22 13:07:18 -0500

    A

--- Control.php
+++ Control.php
@@ -1,1 +1,12 @@
-Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur ab quibusdam saepe quos vitae aut, omnis perspiciatis ducimus et numquam iste iusto, veniam, odit unde esse. Fuga vitae animi assumenda.
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur ab quibusdam saepe quos vitae aut, omnis perspiciatis ducimus et numquam iste iusto, veniam, odit unde esse. Fuga vitae animi assumenda.
+    </body>
+</html>
